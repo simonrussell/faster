82f3349cfe4e0050880b36ea9e284a59a594b5e9
Simon Russell <simon@bellyphant.com>
2013-05-22 22:13:06 +1000

    Some basic functionality.

--- ext/faster_mri/faster_mri.c
+++ ext/faster_mri/faster_mri.c
@@ -3,8 +3,145 @@
 VALUE Faster = Qnil;
 VALUE Faster_Array = Qnil;
 
+typedef struct
+{
+  long capacity;
+  long length;
+  int *items;
+} IntArray;
+
+static IntArray *IntArray_new(void)
+{
+  IntArray *int_array = malloc(sizeof(IntArray));
+
+  int_array->items = NULL;
+  int_array->capacity = int_array->length = 0;
+
+  return int_array;
+}
+
+static void IntArray_resize(IntArray *int_array, long new_capacity)
+{
+  int *new_items;
+
+  if (new_capacity > 0)
+  {
+    new_items = realloc(int_array->items, sizeof(int) * new_capacity);
+
+    if (new_items)
+    {
+      int_array->items = new_items;
+      int_array->capacity = new_capacity;
+
+      if (new_capacity < int_array->length)
+      {
+        int_array->length = new_capacity;
+      }
+    }
+  }
+  else
+  {
+    if (int_array->items)
+    {
+      free(int_array->items);
+    }
+
+    int_array->items = NULL;
+    int_array->capacity = int_array->length = 0;
+  }
+}
+
+static void IntArray_delete(IntArray *int_array)
+{
+  IntArray_resize(int_array, 0);
+  free(int_array);
+}
+
+static VALUE Int_allocate(VALUE klass)
+{
+  IntArray *int_array = IntArray_new();
+
+  return Data_Wrap_Struct(klass, NULL, IntArray_delete, int_array);
+}
+
+static VALUE Int_initialize(VALUE self, VALUE items)
+{
+  IntArray *int_array;
+  long items_length;
+  long i;
+
+  Data_Get_Struct(self, IntArray, int_array);
+
+  Check_Type(items, T_ARRAY);
+  items_length = RARRAY_LEN(items);
+
+  // resize _might_ fail, and leave it unmodified
+  IntArray_resize(int_array, items_length);
+
+  if (items_length < int_array->capacity)
+  {
+    int_array->length = items_length;
+  }
+  else
+  {
+    int_array->length = int_array->capacity;
+  }
+
+  // copy the items that'll fit
+  for (i = 0; i < int_array->length; i++)
+  {
+    int_array->items[i] = NUM2INT(RARRAY_PTR(items)[i]);
+  }
+
+  return self;
+}
+
+static VALUE Int_at(VALUE self, VALUE rbIndex)
+{
+  IntArray *int_array;
+  Data_Get_Struct(self, IntArray, int_array);
+
+  Check_Type(rbIndex, T_FIXNUM);
+  int index = NUM2INT(rbIndex);
+
+  if (index >= 0)
+  {
+    if (index < int_array->length)
+    {
+      return INT2NUM(int_array->items[index]);
+    }
+    else
+    {
+      return Qnil;
+    }
+  }
+  else
+  {
+    return Qnil;
+  }
+}
+
+static VALUE Int_length(VALUE self)
+{
+  IntArray *int_array;
+  Data_Get_Struct(self, IntArray, int_array);
+
+  return LONG2NUM(int_array->length);
+}
+
+static void define_Faster_Array_Int(void)
+{
+  VALUE klass = rb_define_class_under(Faster_Array, "Int", rb_cObject);
+  rb_define_alloc_func(klass, Int_allocate);
+  rb_define_method(klass, "initialize", Int_initialize, 1);
+  rb_define_method(klass, "at", Int_at, 1);
+  rb_define_method(klass, "length", Int_length, 0);
+}
+
 void Init_faster_mri(void)
 {
   Faster = rb_define_module("Faster");
   Faster_Array = rb_define_module_under(Faster, "Array");
+
+  define_Faster_Array_Int();
 }
--- spec/faster/array_spec.rb
+++ spec/faster/array_spec.rb
@@ -3,15 +3,36 @@ require 'spec_helper'
 describe Faster::Array do
 
   shared_examples "an array" do
+    let(:array) { klass.new(items) }
 
+    describe "#length" do
+      subject { array.length }
+      it { should == items.length }
+    end
+
+    describe "#at" do
+      context "in array" do
+        it "should work" do
+          (0...items.length).each do |i|
+            array.at(i).should == items[i]
+          end
+        end
+      end
+
+      context "negative index" do
+
+      end
+    end
   end
 
   describe "Ruby Array" do
+    let(:items) { [1,2,3] * 100 }
     let(:klass) { ::Array }
     it_should_behave_like "an array"
   end
 
   describe "int" do
+    let(:items) { [1,2,3] * 100 }
     let(:klass) { Faster::Array::Int }
     it_should_behave_like "an array"
   end
